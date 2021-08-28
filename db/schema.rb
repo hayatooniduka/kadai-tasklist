74e3124ac7beb91a8d433fb00320ba350838cdf2
hayato.oniduka <hayato.onika@gmail.com>
2021-08-28 13:07:55 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_082658) do
+ActiveRecord::Schema.define(version: 2021_08_28_125019) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
