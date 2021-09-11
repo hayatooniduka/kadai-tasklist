59e73a8d3e5f714ec72080ca527bac972ff1b2cb
hayato.oniduka <hayato.onika@gmail.com>
2021-09-11 09:49:51 +0000

    firststep

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy, :update]
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     
     def index
-        @tasks = Task.all
+        if logged_in?
+            @tasks = current_user.tasks.all
+        end
     end
 
     def show
@@ -13,14 +17,14 @@ class TasksController < ApplicationController
     end
 
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = 'Taskが正常に追加されました'
-            redirect_to @task
+            redirect_to root_url
         else
             flash.now[:danger] = 'Taskが追加されませんでした'
-            render :new
+            render 'tasks/new'
         end
     end
     
@@ -54,4 +58,12 @@ class TasksController < ApplicationController
     def task_params
         params.require(:task).permit(:content, :status)
     end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
+    end
+    
 end
