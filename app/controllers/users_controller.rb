bb4474a9728a43b3df990568b772de7b19a502d4
hayato.oniduka <hayato.onika@gmail.com>
2021-09-13 13:07:48 +0000

    update3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.all
-  end
 
   def new
     @user = User.new
@@ -19,7 +9,7 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = 'ユーザを登録しました'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました'
       render :new
